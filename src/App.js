9b08e583563ce8c8a90eea9e96a6f62ec9b8d8fc
Vincent <vincent.desmares@gmail.com>
2017-12-12 20:32:52 -0800

    Removing dead files.

--- src/App.js
+++ src/App.js
@@ -7,18 +7,12 @@ import { Route } from "react-router-dom";
 import ProjectsPage from "./components/page/projects";
 import ProjectPage from "./components/page/project";
 import NewProjectPage from "./components/page/projectCreate";
-import TerrainsPage from "./components/page/terrains";
-import TerrainPage from "./components/page/terrain";
 import ZoningsPage from "./components/page/zonings";
 import ScenesPage from "./components/page/scenes";
 import ScenePage from "./components/page/scene";
 import NewScenePage from "./components/page/sceneCreate";
 import JobsPage from "./components/page/jobs";
 import Header from "./components/header";
-
-import "aframe";
-import "aframe-particle-system-component";
-import "aframe-terrain-model-component";
 
 class App extends Component {
   render() {
@@ -34,18 +28,8 @@ class App extends Component {
         />
         <Route
           exact
-          path="/project/:projectId/terrain"
-          component={TerrainsPage}
-        />
-        <Route
-          exact
           path="/project/:projectId/zoning"
           component={ZoningsPage}
-        />
-        <Route
-          exact
-          path="/project/:projectId/terrain/:terrainId([0-9]+)"
-          component={TerrainPage}
         />
         <Route exact path="/project/:projectId/scene" component={ScenesPage} />
         <Route
